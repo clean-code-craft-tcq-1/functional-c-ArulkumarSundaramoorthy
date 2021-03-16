7068e99d42bec1e472abc5c75b299bf178281e3e
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-03-16 20:03:44 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -22,7 +22,7 @@ int main() {
 	assert(!batteryIsOk(50, 85, 0.1));	
 	assert(!batteryIsOk(50, 70, 0.7));
 	assert(!batteryIsOk(25, 85, 0.7));
-	assert(!batteryIsOk(25, 70, 0.1));
+	assert(!batteryIsOk(25, 71, 0.1));
 
     return 0;
 }
